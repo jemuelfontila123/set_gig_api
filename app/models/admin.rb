5c5957c90fdf4adb09914aaee5120d81fb125bb0
jemuelfontila123 <jemuelfontila123@gmail.com>
2023-07-23 09:20:48 +0800

    added password complexity for admin and added tests

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,10 @@
 class Admin < ApplicationRecord 
   has_secure_password 
   
-  # Add password complexity later on
+  
+  include ActiveModel::Validations
+  validates_with Validators::AdminValidator 
   validates :password, length: {minimum: 10}
   validates :username, uniqueness: true, presence: true
+  
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe Admin, type: :model do 
 
   subject {
-    described_class.new(username: "anything", password: "minimumisten")
+    described_class.new(username: "anything", password: "Minimumisten1#")
   }
 
   it "is valid with valid attributes" do 
@@ -25,5 +25,20 @@ RSpec.describe Admin, type: :model do
     subject.password = "123456789"
     expect(subject).not_to be_valid 
   end 
+  
+  it "is not valid if the password has no 1 uppercase" do 
+    subject.password = "minimumisten1#"
+    expect(subject).not_to be_valid 
+  end
+
+  it "is not valid if the password has no 1 special character" do 
+    subject.password = "Minimumisten1"
+    expect(subject).not_to be_valid 
+  end
+
+  it "is not valid if the password has no 1 number" do 
+    subject.password = "Minimumisten#"
+    expect(subject).not_to be_valid 
+  end
 
 end
