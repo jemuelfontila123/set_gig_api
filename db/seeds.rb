a11e4150e1da3a989d39624baadeffa04a025b2f
jemuelfontila123 <jemuelfontila123@gmail.com>
2023-07-29 13:35:19 -0400

    Added Seed of Production Schedule

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+#Seed DB with 1 admin user
+admin = Admin.find_or_initialize_by(username: "superadmin")
+unless admin.persisted? 
+  admin.password = "Minimumisten1#"
+  admin.save 
+end
+
+
+#Create A month of Production Schedule  
+start_date = Date.today 
+end_date = Date.today.end_of_month + 1.month 
+dates = (start_date .. end_date).to_a.select {|date| date.on_weekend? } 
+dates.each do |date| 
+  start_time = DateTime.new(date.year, date.month, date.day, 19)
+  end_time = (start_time + 1.day).change(hour: 1)
+  date = Schedule.find_or_initialize_by(start_time: start_time, end_time: end_time,  schedule_type: Schedule.schedule_types[:production])
+  date.save unless date.persisted?
+end
+
+#Create a month of band schedule
+# dates = (start_date..end_date).to_a.select {|date| date.on_weekday?}
+# dates.each do |date| 
+
+#Create Booking - Band/Production
+
+#Create Online Link 
+
+
+#Create Tentative Lineup
+
+#Create Contact Information
+
+
