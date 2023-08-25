177f3006490cf4e779cb087353cc3d846269cfb7
jemuelfontila123 <jemuelfontila123@gmail.com>
2023-08-25 17:20:25 -0400

    added dotenv and added temporarily closing schedule when booking

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,12 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# Load dotenv only in development or test environment
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
+HOSTNAME = ENV['HOSTNAME']
 module SetGig
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -25,7 +30,6 @@ module SetGig
     # config.autoload_paths += Dir[File.join(Rails.root, 'app', 'models', 'validators
 
     config.api_only = true
-
     
     # config.active_record.default_timezone = :local
     # config.time_zone = "Asia/Manila"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,16 +59,23 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-  config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i'
   # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV["SMTP_EMAIL"]}
 
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => ENV["SMTP_ADDRESS"],
+    :port                 => 587,
+    :user_name            => ENV["SMTP_EMAIL"],
+    :password             => ENV["SMTP_PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
